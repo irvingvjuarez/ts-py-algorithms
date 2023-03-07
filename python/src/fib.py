0e5a86632f444609be8bf048bb79d3346321eb94
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-07 10:24:19 -0600

    Fibonacci sequence added in python file

--- python/src/fib.py
+++ python/src/fib.py
@@ -1,14 +1,20 @@
-def fib(index: int):
+import sys
+sys.setrecursionlimit(10000)
+
+def fib(index: int, obj: dict = {}):
 	assert type(index) == int, f"{index} must be an integer"
 
 	if index <= 2:
 		return 1
+	elif index in obj:
+		return obj[index]
 
-	return fib(index - 1) + fib(index - 2)
+	obj[index] = fib(index - 1, obj) + fib(index - 2, obj)
+	return obj[index]
 
 if __name__ == "__main__":
 	print(
-		fib(12) # 144
+		fib(1200)
 	)
 
 	print(
