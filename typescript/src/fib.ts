89811bfef2912de52dc95604d3df68980cf8999e
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-07 09:57:48 -0600

    TS implementation of the fib series done

---
+++ typescript/src/fib.ts
@@ -0,0 +1,13 @@
+function fib(index: number): number {
+	if (index <= 2) return 1
+
+	return fib(index - 1) + fib(index - 2)
+}
+
+console.log(
+	fib(12) // 144
+)
+
+console.log(
+	fib(13) // 233
+)
