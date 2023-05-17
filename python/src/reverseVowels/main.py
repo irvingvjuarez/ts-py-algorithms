53d51a7c0330079e31a4b614c11a162383865406
Irving Juárez <irvingjuarez274@gmail.com>
2023-05-16 19:36:47 -0500

    [ADD] code solution

---
+++ python/src/reverseVowels/main.py
@@ -0,0 +1,23 @@
+vowels = list("aeiouAEIOU")
+
+def reverse_vowels(s: str):
+	s = list(s)
+	head = 0
+	tail = len(s) - 1
+
+	while head < tail:
+		if s[head] in vowels and s[tail] in vowels:
+			# SWAP
+			head_intermediary = s[head]
+			s[head] = s[tail]
+			s[tail] = head_intermediary
+
+			head += 1
+			tail -= 1
+
+		elif s[head] in vowels:
+			tail -= 1
+		elif s[tail] in vowels:
+			head += 1
+
+	return "".join(s)
