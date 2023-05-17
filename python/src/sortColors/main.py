46f5664982d1d22a3c3ace4a082aa7022b592656
Irving Juárez <irvingjuarez274@gmail.com>
2023-05-16 19:08:36 -0500

    [ADD] code solution, ready to be tested

---
+++ python/src/sortColors/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def sortColors(nums: List[int]):
+	head = 0
+	tail = len(nums) - 1
+
+	while head < tail:
+		head_stationary = None
+
+		if nums[tail] < nums[head]:
+			head_stationary = nums[head]
+			nums[head] = nums[tail]
+			nums[tail] = head_stationary
+
+			tail -= 1
+		elif nums[tail] > nums[head]:
+			head += 1
+		else:
+			tail -= 1
+
+	return nums
