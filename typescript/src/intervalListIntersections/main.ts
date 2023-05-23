95f07a1ea7152db7a6d01ea0afdb4b4a4ad6caca
Irving Juárez <irvingjuarez274@gmail.com>
2023-05-23 13:31:58 -0600

    [UPDATE] main.ts page, algorithm finished, but debugging missing

--- typescript/src/intervalListIntersections/main.ts
+++ typescript/src/intervalListIntersections/main.ts
@@ -23,10 +23,70 @@ class IntervalIntersecter {
 			secondList[secondList.length - 1][1]
 		)
 
+		this.setupIntervals()
+
+		this.pointer = Math.min(this.int1.head(), this.int2.head())
+	}
+
+	public solve() {
+		const intersections: IntervalList = []
+		let currentOverlap: number[] = []
+		let currentOverlapFirstIndex = true
+
+		while (this.limit > this.pointer) {
+			let overlap = this.isOverlapping()
+
+			if (overlap) {
+				if (currentOverlapFirstIndex) {
+					currentOverlapFirstIndex = false
+					currentOverlap.push(this.pointer, this.pointer)
+				} else {
+					currentOverlap[1] = this.pointer
+				}
+			} else {
+				if (currentOverlap.length) {
+					intersections.push(currentOverlap)
+				}
+
+				currentOverlap = []
+				currentOverlapFirstIndex = true
+			}
+
+			this.movePointerForward()
+		}
+
+		return intersections
+	}
+
+	protected movePointerForward() {
+		this.pointer++
+
+		if (this.pointer > this.int1.tail()) {
+			this.updateInterval(1)
+		}
+
+		if (this.pointer > this.int2.tail()) {
+			this.updateInterval(2)
+		}
+	}
+
+	protected updateInterval(intervalId: number) {
+		if (intervalId === 1) {
+			this.int1.index++
+		} else {
+			this.int2.index++
+		}
+	}
+
+	protected isOverlapping() {
+		return this.pointer <= this.int1.tail() && this.pointer >= this.int2.head()
+	}
+
+	protected setupIntervals() {
 		this.int1 = {
 			index: 0,
 			value: function() {
-				return firstList[this.index]
+				return this.firstList[this.index]
 			},
 			head: function() {
 				return this.value()[0]
@@ -39,7 +99,7 @@ class IntervalIntersecter {
 		this.int2 = {
 			index: 0,
 			value: function() {
-				return secondList[this.index]
+				return this.secondList[this.index]
 			},
 			head: function() {
 				return this.value()[0]
@@ -48,8 +108,6 @@ class IntervalIntersecter {
 				return this.value()[1]
 			}
 		}
-
-		this.pointer = Math.min(this.int1.head(), this.int2.head())
 	}
 }
 
