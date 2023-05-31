f68383da8ca3c18f9b0910b04ccb830a52abd20f
Irving Juárez <irvingjuarez274@gmail.com>
2023-05-30 19:53:16 -0500

    [UPDATE] tests

--- typescript/src/findAllAnagramsInAString/main.test.ts
+++ typescript/src/findAllAnagramsInAString/main.test.ts
@@ -3,11 +3,13 @@ import getAnagrams from "./main"
 
 describe("Tests of getAnagrams", () => {
 	test("Existence", async () => {
-
+		await expect(getAnagrams).toBeDefined()
 	})
 
 	test("Correctness", async () => {
+		await expect(getAnagrams("cbaebabacd", "abc")).toEqual([0,6])
 
+		await expect(getAnagrams("abab", "ab")).toEqual([0,1,2])
 	})
 
 	test("Failure", async () => {
